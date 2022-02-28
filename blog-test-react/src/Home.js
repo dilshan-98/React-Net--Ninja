862d3fb6f512b34caa68524318f6aeca7d39d2d1
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-02-28 05:37:49 +0530

    P2 - props...

--- blog-test-react/src/Home.js
+++ blog-test-react/src/Home.js
@@ -1,23 +1,19 @@
-import { useState} from "react";
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [name, setName] = useState("Dinusha");
+    const [blogs, setBlogs] = useState([
+        { title: "New Website 0", body: "Lorem ipsum...", author: "Author 1", id: 1 },
+        { title: "New Website 1", body: "Lorem ipsum...", author: "Author 2", id: 2 },
+        { title: "New Website 2", body: "Lorem ipsum...", author: "Author 3", id: 3 },
+        { title: "New Website 3", body: "Lorem ipsum...", author: "Author 4", id: 4 },
+        { title: "New Website 4", body: "Lorem ipsum...", author: "Author 5", id: 5 }
+    ]);
 
-
-    const handleClick = () =>{
-        setName("Dilshan");
-    }
-
-    const handleClickAgain = (name, e) =>{
-        console.log("Hello "+name, e.target);
-    }
-
-    return(
+    return (
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click Me</button>
+            <BlogList blogs={blogs}></BlogList>
         </div>
     );
 }

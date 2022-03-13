0a8e112731ce8ae59ce25ec7f902807a16a3fe3b
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-03-13 17:59:56 +0530

    initializing react router dom

--- blog-test-react/src/App.js
+++ blog-test-react/src/App.js
@@ -1,16 +1,22 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
 
   return (
-    <div className="App">
-      <div className="content">
+    <Router>
+      <div className="App">
         <Navbar />
-        <Home />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
+
 }
 
 export default App;

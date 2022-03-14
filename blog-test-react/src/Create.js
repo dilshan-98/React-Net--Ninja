0940577f91c254e8f95ac59eef1917a964599e5a
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-03-15 01:28:57 +0530

    initiated form input states

--- blog-test-react/src/Create.js
+++ blog-test-react/src/Create.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("James");
+
     return(
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title: </label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Description: </label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+                <label>Blog Author: </label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="James">James</option>
+                    <option value="Lori">Lori</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     )
 }

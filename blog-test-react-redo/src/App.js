883ed2746a2168bddbdde3769e1dc6d46356b295
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-04-03 23:30:32 +0530

    404 page

--- blog-test-react-redo/src/App.js
+++ blog-test-react-redo/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import BlogDetails from "./BlogDetails";
 import Create from "./Create";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/blogs/:id" element={<BlogDetails />} />
             <Route path="/create" element={<Create />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>

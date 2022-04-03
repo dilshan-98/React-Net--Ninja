0769e676eff486d9928c55ac3c59a833810f8646
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-04-03 23:21:37 +0530

    Delete Blog

--- blog-test-react-redo/src/BlogDetails.js
+++ blog-test-react-redo/src/BlogDetails.js
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
     const { blogs, isPending, error} = useFetch("http://localhost:5000/blogs/"+id);
+    
+    const navigate = useNavigate();
 
     const deleteHandler = () => {
-        
+        fetch("http://localhost:5000/blogs/"+id, {
+            method: "DELETE"
+        })
+        .then(() => {
+            console.log("Blog Deleted Successfully");
+            navigate("/");
+        })
+        .catch(error => console.log(error.message));
     }
 
     return (
@@ -18,7 +28,7 @@ const BlogDetails = () => {
                     <h2>{blogs.title}</h2>
                     <p>Written by {blogs.author}</p>
                     <div>{blogs.body}</div>
-                    <button onClick={(e) => deleteHandler()}>Delete</button>
+                    <button onClick={deleteHandler}>Delete</button>
                 </article>    
             }
         </div>
